ab8e0b0c9f09d3571bd0f9e9bf7f3300e96ec904
Thanatmet-Arcadia <monchai.t@arcadiaapm.com>
2023-10-29 11:56:35 +0700

    initial learn next

---
+++ app/ui/fonts.ts
@@ -0,0 +1,16 @@
+import { Inter, Lusitana } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+
+export const lusitana = Lusitana({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
+// For font thai
+// import { Kanit } from "next/font/google";
+
+// export const kanit = Kanit({
+//   subsets: ["thai"],
+//   weight: "400",
+// });
